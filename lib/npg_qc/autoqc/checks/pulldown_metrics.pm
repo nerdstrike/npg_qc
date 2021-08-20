15bed71c6af43185c91940eeff44680bcdc75228
nerdstrike <kt19@sanger.ac.uk>
2021-08-20 11:30:07 +0100

    Add more error catching, POD, tidy up

--- lib/npg_qc/autoqc/checks/pulldown_metrics.pm
+++ lib/npg_qc/autoqc/checks/pulldown_metrics.pm
@@ -6,7 +6,6 @@ use English qw( -no_match_vars );
 use Carp;
 use File::Spec::Functions qw( catdir catfile );
 use Readonly;
-use Try::Tiny;
 
 extends qw(npg_qc::autoqc::checks::check);
 with 'npg_tracking::data::bait::find',
@@ -15,10 +14,11 @@ with 'npg_tracking::data::bait::find',
 
 our $VERSION = '0';
 
-Readonly::Scalar my $PICARD_COMMAND    => q[CollectHsMetrics];
+Readonly::Scalar my $PICARD_METRICS_NAME => q[CollectHsMetrics];
 Readonly::Scalar my $MAX_JAVA_HEAP_SIZE => q[3000m];
 Readonly::Scalar my $MINUS_ONE          => -1;
 Readonly::Scalar my $MIN_ON_BAIT_BASES_PERCENTAGE => 20;
+Readonly::Scalar my $GATK_OUTPUT_SUFFIX => '.gatk_collecthsmetrics.txt';
 
 Readonly::Hash   my %PICARD_METRICS_FIELDS_MAPPING => {
     'BAIT_TERRITORY'       => 'bait_territory',
@@ -57,13 +57,6 @@ has 'max_java_heap_size' => (
     default => $MAX_JAVA_HEAP_SIZE,
 );
 
-has 'picard_module' => (
-    is      => 'ro',
-    isa     => 'Str',
-    default => $PICARD_COMMAND,
-);
-
-# TODO: This needs to be factored out of here, insert_size and alignment_filter_metrics
 has 'reference' => (
     is => 'ro',
     isa => 'Str',
@@ -72,22 +65,28 @@ has 'reference' => (
 
 sub _build_reference {
     my $self = shift;
-    my $ref;
-    try {
-        $ref = pop @{$self->refs()};
+    if (@{$self->refs} > 1) {
+        croak 'More than one reference supplied. Cannot run pulldown_metrics with ambiguous reference FASTA';
     }
-    catch {
-        $self->result->add_comment(
-            q[Error: binary reference cannot be retrieved; cannot run insert size check. ] . $_
-        );
-    };
+    my $ref = pop @{$self->refs()};
     if (!$ref) {
-        $self->result->add_comment('Failed to retrieve reference');
+        croak 'No reference supplied. Cannot interpret CRAM without a reference FASTA file';
     }
     return $ref;
 }
 
-has 'picard_arguments' => (
+has 'output_file' => (
+    is => 'ro',
+    isa => 'Str',
+    lazy_build => 1,
+);
+
+sub _build_output_file {
+    my $self = shift;
+    return catfile($self->qc_out->[0], $self->filename_root.$GATK_OUTPUT_SUFFIX);
+}
+
+has '_picard_arguments' => (
     is => 'ro',
     isa => 'ArrayRef[Str]',
     lazy_build => 1,
@@ -96,6 +95,7 @@ has 'picard_arguments' => (
 sub _build_picard_arguments {
     my $self = shift;
     return [
+        '--VALIDATION_STRINGENCY=SILENT',
         '--BAIT_INTERVALS='.$self->bait_intervals_path,
         '--TARGET_INTERVALS='.$self->target_intervals_path,
         '--REFERENCE_SEQUENCE='.$self->reference,
@@ -140,20 +140,19 @@ override 'execute' => sub {
         return 1;
     }
 
-    $self->result->set_info( 'Aligner', 'Picard '.$self->picard_module );
+    $self->result->set_info( 'Picard_metrics_name', 'Picard '.$PICARD_METRICS_NAME );
     $self->result->set_info( 'Aligner_version', $self->current_version($self->gatk_cmd) );
     $self->result->bait_path($self->bait_path);
-    local $ENV{JAVA_TOOL_OPTIONS} = '-Xmx'.$self->max_java_heap_size;
-    my $exit = system
+    my $exit = system join q[ ],
         $self->gatk_cmd,
-        $self->picard_module,
-        @{$self->picard_arguments};
+        '-Xmx'.$self->max_java_heap_size,
+        $PICARD_METRICS_NAME,
+        @{$self->_picard_arguments};
     if ($exit != 0) {
-        croak 'Failed to run GATK with '.join ', ', $self->picard_module, @{$self->picard_arguments};
+        croak 'Failed to run GATK with '.join ', ', $PICARD_METRICS_NAME, @{$self->_picard_arguments};
     }
-    ## no critic (InputOutput::RequireBriefOpen Variables::ProhibitPunctuationVars)
-    open my $fh, '<', $self->output_file or croak 'Failed to open GATK output file: '.$self->output_file.q{ }.$?;
-    ## use critic
+
+    open my $fh, '<', $self->output_file or croak 'Failed to open GATK output file: '.$self->output_file.q{ }.$CHILD_ERROR;
     my $results = $self->_parse_metrics($fh);
     close $fh or croak 'File handle close error';
 
@@ -172,16 +171,6 @@ override 'execute' => sub {
     return 1;
 };
 
-has 'output_file' => (
-    is => 'ro',
-    isa => 'Str',
-    lazy_build => 1,
-);
-
-sub _build_output_file {
-    my $self = shift;
-    return catfile($self->qc_out->[0], $self->filename_root.'_gatk_collecthsmetrics.txt');
-}
 
 sub _parse_metrics {
     my ($self, $fh) = @_;
@@ -198,6 +187,9 @@ sub _parse_metrics {
     $lines[0] = <$fh>;
     $lines[1] = <$fh>;
     chomp @lines;
+    if (! ($lines[0] and $lines[1])) {
+        croak q[Pulldown metrics output file is malformed];
+    }
 
     my @keys = split /\t/smx, $lines[0];
     my @values = split /\t/smx, $lines[1], $MINUS_ONE;
@@ -276,11 +268,25 @@ npg_qc::autoqc::checks::pulldown_metrics
 
     Moose-based.
 
+=head2 alignments_in_bam
+
+Setting from LIMS on whether alignment data is available. This check cannot
+run without it. Set when testing to avoid tracking server lookups.
+
 =head2 max_java_heap_size
 
-=head2 picard_module
+Java heap setting for GATK. GATK will allocate too much on its own initiative
 
-=head2 picard_command
+=head2 reference
+
+A path to reference FASTA required to interpret CRAM files.
+
+=head2 output_file
+
+A path for GATK to output pulldown metrics to.
+Required because GATK corrupts CalculateHsMetrics output to STDOUT, and
+customers may wish to inspect the full set of metrics after seeing the QC
+result.
 
 =head1 DIAGNOSTICS
 
